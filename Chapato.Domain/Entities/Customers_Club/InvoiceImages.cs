95d34d577bd1652e98246d09404e1a6e8356f1bc
mvm1994 <m.moqadam1994@gmail.com>
2024-11-26 23:24:31 +0330

    invoice_preview_and_invoice_print

---
+++ Chapato.Domain/Entities/Customers_Club/InvoiceImages.cs
@@ -0,0 +1,15 @@
+﻿using Chapato.Domain.Entities.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapato.Domain.Entities.Customers_Club
+{
+    public class InvoiceImages:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Src { get; set; }
+    }
+}
---
+++ EndPoint.Site/Areas/Admin/Controllers/InvoiceController.cs
@@ -0,0 +1,35 @@
+﻿using Chapato.Application.Interfaces.FacadPatterns;
+using Chapato.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class InvoiceController : Controller
+    {
+        //private readonly IProductFacad _productFacad;
+        private readonly ModeCookie _modeCookie;
+        public InvoiceController(ModeCookie modeCookie)
+        {
+            _modeCookie = modeCookie;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Mode = _modeCookie.GetCookieValue("ckmode");
+            return View();
+        }
+
+        
+        public IActionResult InvoicePreview()
+        {
+            ViewBag.Mode = _modeCookie.GetCookieValue("ckmode");
+            return View();
+        }
+
+        public IActionResult InvoicePrint()
+        {
+            ViewBag.Mode = _modeCookie.GetCookieValue("ckmode");
+            return View();
+        }
+    }
+}
